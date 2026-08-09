9b5cbbdb8ea11639d01674b647b177717d7bd6bf
AMAN SINGH <2amansingh07@gmail.com>
2026-08-09 23:41:15 +0530

    feat(backend): added html to upload docs

--- apps/backend-api/src/server.ts
+++ apps/backend-api/src/server.ts
@@ -1,6 +1,6 @@
 import server from './app.js';
 import { config } from './config/env.js';
 
-server.listen(config.port, () => {
-  console.log(`⚡ [smartFill Backend API] Server running on http://localhost:${config.port}`);
+server.listen(Number(config.port), '0.0.0.0', () => {
+  console.log(`⚡ [smartFill Backend API] Server running on http://0.0.0.0:${config.port}`);
 });
